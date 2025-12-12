5c9f6b45966d35abf30edc1f03b7c0b892c0a402
Cacton12 <Colbyacton12@icloud.com>
2025-12-12 15:26:17 -0400

    better quality images

--- StorageWebAppBackend/Services/ImageService.cs
+++ StorageWebAppBackend/Services/ImageService.cs
@@ -1,6 +1,7 @@
 ﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -8,7 +9,6 @@ namespace StorageWebAppBackend.Services
 {
     public class ImageService
     {
-        // Resize image to max width/height while maintaining aspect ratio
         public async Task<Stream> ResizeImageAsync(Stream inputStream, int maxWidth = 2560, int maxHeight = 1440)
         {
             var image = await Image.LoadAsync(inputStream);
@@ -16,11 +16,18 @@ namespace StorageWebAppBackend.Services
             image.Mutate(x => x.Resize(new ResizeOptions
             {
                 Mode = ResizeMode.Max,
-                Size = new Size(maxWidth, maxHeight)
+                Size = new Size(maxWidth, maxHeight),
+                Sampler = KnownResamplers.Lanczos3 // high‑quality resampling
             }));
 
             var outputStream = new MemoryStream();
-            var encoder = new JpegEncoder { Quality = 80 };
+            var encoder = new JpegEncoder
+            {
+                Quality = 95,
+                // Use RGB encoding to preserve full color fidelity
+                ColorType = JpegEncodingColor.Rgb
+            };
+
             await image.SaveAsJpegAsync(outputStream, encoder);
             outputStream.Position = 0;
             return outputStream;
